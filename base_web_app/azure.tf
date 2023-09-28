ed83f1c1b1102f0212c93267718f4e20ced6610b
lex885uk <lex885@gmail.com>
2023-09-28 20:11:55 +0100

    first

---
+++ base_web_app/azure.tf
@@ -0,0 +1,30 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+#   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "GSWTF" {
+  name     = "globo-app-resource-Group"
+  location = "UK South"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "GSWTF" {
+  name                = "globo-app-vnet"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}

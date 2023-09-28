9cf2f9a09d0b212540d37c0c0e48bb6e57cfc0b5
lex885uk <lex885@gmail.com>
2023-09-28 21:07:46 +0100

    fourth

--- base_web_app/azure.tf
+++ base_web_app/azure.tf
@@ -29,6 +29,49 @@ resource "azurerm_virtual_network" "GSWTF" {
   address_space       = ["10.0.0.0/16"]
 }
 
+resource "azurerm_windows_virtual_machine" "GSWTF" {
+  name                     = "globo-app-vm"
+  resource_group_name      = azurerm_resource_group.GSWTF.name
+  location                 = azurerm_resource_group.GSWTF.location
+  size                     = "Standard_DS1_v2"
+  admin_username           = "gswtfadmin"
+  admin_password           = "Password1234!"
+  network_interface_ids    = [azurerm_network_interface.GSWTF.id]
+  computer_name            = "globo-app-vm"
+  enable_automatic_updates = true
+  provision_vm_agent       = true
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+  os_disk {
+    name                 = "globo-app-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  tags = {
+    environment = "gswtf"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "vm_extension_install_iis" {
+  name                       = "vm_extension_install_iis"
+  virtual_machine_id         = azurerm_windows_virtual_machine.GSWTF.id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.8"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
+    }
+SETTINGS
+}
+
+
 resource "azurerm_network_interface" "GSWTF" {
   name                = "globo-app-nic"
   location            = azurerm_resource_group.GSWTF.location

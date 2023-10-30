6b4b2913755a47fcb31eb2d171c518361495f55a
lex885uk <lex885@gmail.com>
2023-10-30 21:20:49 +0000

    aad2

--- base_web_app/azure.tf
+++ base_web_app/azure.tf
@@ -63,21 +63,6 @@ SETTINGS
   tags       = local.common_tags
   depends_on = [azurerm_windows_virtual_machine.gswtf]
 }
-
-resource "azurerm_virtual_machine_extension" "vm_extension_enable_aad" {
-  name                       = "vm_extension_enable_aad"
-  virtual_machine_id         = azurerm_windows_virtual_machine.gswtf.id
-  publisher                  = "Microsoft.Azure.ActiveDirectory"
-  type                       = "AADLoginForWindows"
-  type_handler_version       = "1.0"
-  auto_upgrade_minor_version = true
-
-  settings = <<SETTINGS
-    {
-        "TenantID": "7aa8c601-0c14-4b9d-9818-9d4d0baf1ab1"
-    }
-  
-}
 resource "azurerm_network_interface" "gswtf" {
   name                = var.network_interface.name
   location            = azurerm_resource_group.gswtf.location
@@ -97,4 +82,20 @@ resource "azurerm_public_ip" "gswtf" {
   resource_group_name = azurerm_resource_group.gswtf.name
   location            = azurerm_resource_group.gswtf.location
   allocation_method   = var.public_ip.allocation_method
+}
+
+resource "azurerm_virtual_machine_extension" "vm_extension_enable_aad" {
+  name                       = "vm_extension_enable_aad"
+  virtual_machine_id         = azurerm_windows_virtual_machine.gswtf.id
+  publisher                  = "Microsoft.Azure.ActiveDirectory"
+  type                       = "AADLoginForWindows"
+  type_handler_version       = "1.0"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+        "TenantID": "7aa8c601-0c14-4b9d-9818-9d4d0baf1ab1"
+    }
+
+SETTINGS
 }
